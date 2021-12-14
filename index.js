e65b6aa22e57da7b3f070c4d3b474ef5cbb92e44
Greg Christiansen <gregchristiansen14@gmail.com>
2021-12-13 20:25:55 -0600

    fixed the user input questions

--- index.js
+++ index.js
@@ -1,52 +1,160 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+const util = require('util');
 
-const promptUser = () => {
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager');
+const teamBuilder = require('./src/page-template');
+
+let Team = [];
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptManager = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'number',
-            name: 'id',
-            message: 'What is your id number? (Required)',
-            validate: idNumber => {
-                if (idNumber) {
-                    return true;
-                } else {
-                    console.log('Please enter your id number!');
-                    return false;
-                }
-            }
+            name: 'managerName',
+            message: 'Enter Team managers name:',
         },
         {
             type: 'input',
-            name: 'email',
-            message: 'What is your email address? (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address!')
-                    return false;
-                }
-            }
+            name: 'employeeId',
+            message: 'Enter Managers employee id:',
+        },
+        {
+            type: 'input',
+            name: 'emailAddress',
+            message: 'Enter Email address:',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter office number:'
         },
     ]);
 };
 
-promptUser();
+function optionList() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'Options',
+            message: 'Add engineer, intern or finish building your team:',
+            choices: ['Engineer', 'Intern', "Finish"],
+        },
+    ])
+    .then(teamChoices => {
+        switch (teamChoices.Options) {
+            case 'Engineer':
+                addEng();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+                default:
+                generateTeam();
+        }
+    });
+}
+
+function addEng() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engName',
+            message: 'Enter engineers name:',
+        },
+        {
+            type: 'input',
+            name: 'engId',
+            message: 'Enter engineers ID:',
+        },
+        {
+            type: 'input',
+            name: 'engEmail',
+            message: 'Add engineers email address:',
+        },
+        {
+            type: 'input',
+            name: 'engGithub',
+            message: 'Add engineers Github Username:',
+        },
+    ])
+    .then(answers => {
+        const engineer = new Engineer(
+            answers.engName,
+            answers.engId,
+            answers.engEmail,
+            answers.engGithub
+        );
+        Team.push(engineer);
+
+        optionList();
+    });
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'Add interns name:',
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: 'Add interns ID:',
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'Add interns email:',
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'Add interns school:',
+        },
+    ])
+    .then(answers => {
+        const intern = new Intern(
+            answers.internName,
+            answers.internId,
+            answers.internEmail,
+            answers.internSchool,
+        );
+        Team.push(intern);
+
+        optionList();
+    });
+}
+
+const init = () => {
+    promptManager().then(answers => {
+        const manager = new Manager(
+            answers.managerName,
+            answers.employeeId,
+            answers.emailAddress,
+            answers.officeNumber
+        );
+        Team.push(manager);
+
+        optionList();
+    });
+};
+
+teamBuilder(Team);
+
+const generateTeam = () => {
+    let team = teamBuilder(Team);
+
+    fs.writeFileAsync('..dist/team.html', team, 'utf-8')
+};
+
+init();
 
 
 
@@ -58,8 +166,11 @@ promptUser();
 
 
 
-const profileDataArgs = process.argv.slice(2);
-const [email, github] = profileDataArgs;
+
+
+
+
+
 
 /*fs.writeFile('./index.html', generatePage(email, github), err => {
   if (err) throw new Error(err);
