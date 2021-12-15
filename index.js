ddf3298a8532c25ad32fd723c4355e5037ba742c
Greg Christiansen <gregchristiansen14@gmail.com>
2021-12-14 18:23:41 -0600

    deleted an unused file

--- index.js
+++ index.js
@@ -173,8 +173,3 @@ init();
 
 
 
-/*fs.writeFile('./index.html', generatePage(email, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});*/
